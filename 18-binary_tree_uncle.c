d50d2b06a37536d79758cde58910adad50312a91
mugane <muganes25@gmail.com>
2023-03-30 05:24:51 -0700

    0x12

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - checks if current node has an uncle.
+ *
+ * @node: current node.
+ * Return: A pointer t the uncle node, otherwise NULL.
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->parent->left == node->parent &&
+			node->parent->parent->right)
+		return (node->parent->parent->right);
+
+	if (node->parent->parent->right == node->parent &&
+			node->parent->parent->left)
+		return (node->parent->parent->left);
+
+	return (NULL);
+}
